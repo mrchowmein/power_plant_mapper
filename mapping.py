e619656ec8a5e4b6f34444da2067ee5ebfe3e748
jason <jasonfchan@gmail.com>
2020-06-08 07:08:34 -0700

    start merge

--- mapping.py
+++ mapping.py
@@ -1,29 +1,92 @@
 import pandas as pd
 import hashlib
-import timeit
-
-def remove_paren(country):
-    return country.split('(')[0].strip()
+import numpy as np
+import re
 
 
+def find_intersection(col_1, col_2):
+    return np.intersect1d(col_1, col_2)
+
+def create_hashed_id(sourcedf,destinationdf,*column):
+    columnName = ''
+    destinationdf['hashed_id'] = pd.DataFrame(sourcedf[list(column)].values.sum(axis=1))[0].str.encode('utf-8').apply(lambda x: (hashlib.sha256(x).hexdigest().upper()))
+
+#read in data
 entso_df = pd.read_csv('entso.csv', header = 0)
 platts_df = pd.read_csv('platts.csv', header = 0)
-
 gppd_df = pd.read_csv('gppd.csv', header = 0)
+fuel_thesaurus_df = pd.read_csv('fuel_thesaurus.csv', header = 0)
+
 
 #clean country
 
-entso_df['country'] = entso_df['country'].apply(lambda x: remove_paren(x))
+platts_country_dict = {'UNITED KINGDOM' : 'ENGLAND & WALES', 'NETHERLANDS' : 'dog'}  #Would create a country theasarus if there were more countries
+#entso_df['country'] = entso_df['country'].str.upper()
+entso_df['country'] = entso_df['country'].str.split('(').str[0].str.upper().str.strip()
+entso_df['unit_name'] = entso_df['unit_name'].str.upper()
+entso_df['unit_name'] = entso_df['unit_name'].apply(lambda x: re.sub(r'[^a-zA-Z0-9]+', ' ', x))
+platts_df['UNIT'] = platts_df['UNIT'].apply(lambda x: re.sub(r'[^a-zA-Z0-9]+', ' ', x))
+entso_df['country_platts'] = entso_df['country'].replace('UNITED KINGDOM', 'ENGLAND & WALES')
+
+#entso_df['country'] = entso_df['country'].apply(lambda x: remove_paren(x))
 
 
-fuel_thesaurus_df = pd.read_csv('fuel_thesaurus.csv', header = 0)
-mapping = dict(fuel_thesaurus_df[['unit_fuel_platts_entsoe', 'plant_primary_fuel_gppd']].values)
+#
+# print(country_intersection)
+# print(len(country_intersection))
+
+
+
+
+#mapping = dict(fuel_thesaurus_df[['unit_fuel_platts_entsoe', 'plant_primary_fuel_gppd']].values)
 
 entso_df['unit_fuel'] = entso_df['unit_fuel'].str.lower()
-platts_df['UNIT_FUEL'] = platts_df['UNIT_FUEL'].str.lower()
+#platts_df['UNIT_FUEL'] = platts_df['UNIT_FUEL'].str.lower()
+entso_df['country'] = entso_df['country'].str.upper()
+entso_df['plant_name'] = entso_df['plant_name'].str.upper()
 
-entso_df['unit_fuel'].replace(mapping, inplace = True)
-platts_df['UNIT_FUEL'].replace(mapping, inplace = True)
+#replace, or maybe considering using a merge to create a new column
+#entso_df['unit_fuel'].replace(mapping, inplace = True)
+entso_df = entso_df.merge(fuel_thesaurus_df, left_on='unit_fuel', right_on='unit_fuel_platts_entsoe')
+entso_df['plant_primary_fuel_gppd'] = entso_df['plant_primary_fuel_gppd'].str.upper()
+#print(entso_df[['unit_capacity_mw', 'unit_fuel','unit_fuel_platts_entsoe']])
+
+
+
+#platts_df['UNIT_FUEL'].replace(mapping, inplace = True)
+
+entso_df = entso_df.drop(['unit_capacity_mw', 'plant_capacity_mw', 'unit_fuel_platts_entsoe', 'note'], axis=1)
+
+print(entso_df.info())
+
+country_intersect_platts = find_intersection(platts_df['COUNTRY'].values, entso_df['country_platts'].values)
+
+platts_df = platts_df.drop(['SUBREGION', 'AREA', 'STATE', 'CITY', 'YEAR', 'UNIT_CAPACITY_MW'], axis=1)
+country_intersect_platts = list(country_intersect_platts)
+print(platts_df.info())
+#platts_df = platts_df[platts_df['COUNTRY'].isin(country_intersect_platts)]
+
+print(platts_df.info())
+
+
+#create_hashed_id(entso_df,entso_df,'plant_name', 'country_platts', 'plant_primary_fuel_gppd')
+#create_hashed_id(platts_df,platts_df,'PLANT', 'UNIT_FUEL', 'COUNTRY')
+
+# print(entso_df.iloc[0])
+# print(platts_df.iloc[27596])
+
+# entso_df = entso_df.reset_index(drop=True)
+# platts_df = platts_df.reset_index(drop=True)
+
+output_df = entso_df.merge(platts_df, left_on=['plant_name', 'plant_primary_fuel_gppd', 'country'], right_on=['PLANT', 'UNIT_FUEL', 'COUNTRY'])
+
+#output_df = output_df.query("unit_name == UNIT")
+output_df['unit_match'] = output_df.apply(lambda row: max(row.unit_name.split(), key=len) in row.UNIT and row.unit_name[-1] == row.UNIT[-1], axis=1)
+output_df = output_df[output_df['unit_match'] == True]
+output_df.to_csv("output.csv")
+
+print(output_df.head(5))
+#print(output_df[['plant_primary_fuel_gppd', 'country_platts', 'unit_name']])
 
 
 # df = pd.DataFrame()
@@ -36,13 +99,27 @@ platts_df['UNIT_FUEL'].replace(mapping, inplace = True)
 # print(df_entso.head(5))
 #
 #
-def create_hashed_id(sourcedf,destinationdf,*column):
-    columnName = ''
-    destinationdf['hashed_id'] = pd.DataFrame(sourcedf[list(column)].values.sum(axis=1))[0].str.encode('utf-8').apply(lambda x: (hashlib.sha256(x).hexdigest().upper()))
-
-
-create_hashed_id(entso_df,entso_df,'unit_fuel', 'country', 'unit_name')
-print(entso_df.hashed_id)
 
 
 
+
+#print(entso_df.hashed_id)
+
+#create instersections before processing data, use np vectorization .values
+#then use subsetting
+
+
+
+#platts_country = set(platts_df.COUNTRY.unique())
+
+#entso_country  = set(entso_df.country.unique())
+#
+# print(len(entso_country))
+# print(len(platts_country))
+#
+# print(len(platts_country.intersection(entso_country)))
+#
+
+
+#use merge if using multicolum
+#use join if using index
